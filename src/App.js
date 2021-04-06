1eb233caafb82cc24c613864daa577c14ca4c2ee
VartanovMaks <vartanov2009@gmail.com>
2021-04-06 17:49:54 +0300

    Начало контекста

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Counter = ()=>{
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2 onClick={()=> setCounter(counter+1)}>Счетчик {counter}</h2>
+  )
+}
+const Header = ()=>{
+  
+  return (
+    <h1 onClick={()=> setCounter(counter+1)}>counter</h1>
+  )
+}
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Counter />
     </div>
   );
 }
-
-export default App;
